3afc10364b31e5ba6ba5af73dbd47b45a35955d2
Gurwinder Singh <vargwin@gmail.com>
2021-04-12 21:25:35 +0530

    Startup scripts

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -20,12 +20,6 @@ create table user (
     check role in ('USER', 'ADMIN')
 );
 
-insert into user (first_name, last_name, date_of_birth, email, city, state, country, zip, user_name, password, role)
-values (
-    'the', 'admin', PARSEDATETIME('01-01-2001', 'dd-MM-yyyy'), 'admin@planview.com', 'admin city', 'admin state',
-    'admin country', 'admin zip', 'admin', '$2y$12$PqHYbsHorZGuRQNbI4C7wuUl5LqoNUUTe5Q4TCap/P3Ue1zjRz2Qu', 'ADMIN'
-);
-
 
 drop table if exists message;
 
